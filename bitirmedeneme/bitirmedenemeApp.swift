7aa7cbe1c7274d14afb42ca9e7f4542f537d971d
Beste Kocaoğlu <bestekocaoglu@hotmail.com>
2023-11-18 15:04:12 +0300

    Initial Commit

---
+++ bitirmedeneme/bitirmedenemeApp.swift
@@ -0,0 +1,17 @@
+//
+//  bitirmedenemeApp.swift
+//  bitirmedeneme
+//
+//  Created by Beste Kocaoglu on 18.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct bitirmedenemeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
